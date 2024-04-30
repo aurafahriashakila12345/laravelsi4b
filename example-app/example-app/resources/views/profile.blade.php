c8cde46ed336433e28b3f154f9ecbc2281298c95
aurafahriashakila12345 <fahriashakilaaura@gmail.com>
2024-04-30 16:54:54 +0700

    Install laravel

---
+++ example-app/example-app/resources/views/profile.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>profil</h1>
+    <p>Ini halaman profil</p>
+</body>
+</html>
